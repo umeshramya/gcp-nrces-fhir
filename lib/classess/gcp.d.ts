b5aeac7ee88ee0086abd3a8de26796fad43ca9f0
umeshramya <umeshbilagi@gmail.com>
2021-10-27 17:19:52 +0530

    done'

--- lib/classess/gcp.d.ts
+++ lib/classess/gcp.d.ts
@@ -1,5 +1,6 @@
 export default class GcpFhirCRUD {
     private healthcare;
+    private parent;
     createFhirResource(body: any, resourceType: string): Promise<any>;
 }
 //# sourceMappingURL=gcp.d.ts.map
--- src/classess/gcp.ts
+++ src/classess/gcp.ts
@@ -35,15 +35,31 @@ export default class GcpFhirCRUD {
     headers: { 'Content-Type': 'application/fhir+json' },
   });
 
-
+  private parent: string = `projects/${projectId}/locations/${cloudRegion}/datasets/${datasetId}/fhirStores/${fhirStoreId}`;
 
   async createFhirResource(body: any, resourceType: string) {
 
     try {
-      const parent = `projects/${projectId}/locations/${cloudRegion}/datasets/${datasetId}/fhirStores/${fhirStoreId}`;
-      const request = { parent, type: resourceType, requestBody: body };
+      const request = { parent: this.parent, type: resourceType, requestBody: body };
       const resource: any = await this.healthcare.projects.locations.datasets.fhirStores.fhir.create(request)
+      return resource.data
+    } catch (error) {
+      console.log(error)
+    }
 
+  }
+
+
+
+
+  async deleteFhirResource(resourceId: string, resourceType: string) {
+
+    try {
+      const name = `${this.parent}/fhir/${resourceType}/${resourceId}`
+      const request = { name };
+      const resource: any = await this.healthcare.projects.locations.datasets.fhirStores.fhir..read(
+        request
+      );
 
       return resource.data
     } catch (error) {
