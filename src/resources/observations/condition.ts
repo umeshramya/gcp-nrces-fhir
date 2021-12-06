50c1e4b594bc1141b6d5de7c63e00a47d199c689
pradyumnabilagi <pradyumnabilagi12@gmail.com>
2021-12-06 16:57:47 +0530

    done

--- src/resources/observations/condition.ts
+++ src/resources/observations/condition.ts
@@ -1,26 +1,64 @@
-const body = {
-    "resourceType" : "Condition",
-    "id" : "example-01",
-    "meta" : {
-      "profile" : [
+interface RseourceMaster{
+  getFHIR(options:any):any;
+  convertFhirToObject(options:any):any
+}
+
+export interface CONDITION {
+  text:string;
+  snoemedCode?:string;
+  snowmedDisplay?:string;
+  patientId:string;
+}
+
+export class Condition implements RseourceMaster{
+  getFHIR(options: CONDITION):any {
+
+  const body = {
+    "resourceType": "Condition",
+    "id": "example-01",
+    "meta": {
+      "profile": [
         "https://nrces.in/ndhm/fhir/r4/StructureDefinition/Condition"
       ]
     },
-    "text" : {
-      "status" : "generated",
-      "div" : "<div xmlns=\"http://www.w3.org/1999/xhtml\">Abdominal pain on 09-July 2020</div>"
+    "text": {
+      "status": "generated",
+      "div": options.text
     },
-    "code" : {
-      "coding" : [
+    "code": {
+      "coding": [
         {
-          "system" : "http://snomed.info/sct",
-          "code" : "21522001",
-          "display" : "Abdominal pain"
+          "system": "http://snomed.info/sct",
+          "code": options.snoemedCode,
+          "display": options.snowmedDisplay
         }
       ],
-      "text" : "Abdominal pain"
+      "text": options.text
     },
-    "subject" : {
-      "reference" : "Patient/1"
+    "subject": {
+      "reference": `Patient/${options.patientId}`
     }
+  };
+
+  return body;
+    
   }
+  convertFhirToObject(options: any): CONDITION {
+    let ret:CONDITION={
+      "patientId" :`${options.subject.reference}`.substr(7),
+      "snoemedCode" :options.code.coding[0].code,
+      "text" : options.text,
+      "snowmedDisplay" : options.code.coding[0].display
+    }
+
+    return ret
+  }
+
+
+
+
+
+
+
+
+}
