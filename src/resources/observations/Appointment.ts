45b57f705b291790ad22be738c36f2ceee21c2c2
umeshramya <umeshbilagi@gmail.com>
2021-12-13 11:53:24 +0530

    done

--- src/resources/observations/Appointment.ts
+++ src/resources/observations/Appointment.ts
@@ -1,33 +1,33 @@
 import { CodeDisplay } from "../../config";
 import { ResourceMaster } from "../../Interfaces";
 
-export const AppointmentStatusArray = ["proposed" , "pending" , "booked" , "arrived" , "fulfilled" , "cancelled" , "noshow" , "entered-in-error" , "checked-in" , "waitlist"] as const
+export const AppointmentStatusArray = ["proposed", "pending", "booked", "arrived", "fulfilled", "cancelled", "noshow", "entered-in-error", "checked-in", "waitlist"] as const
 
-type  AppointmentStatus = typeof AppointmentStatusArray[number]
+type AppointmentStatus = typeof AppointmentStatusArray[number]
 
-export const ActorStatusArray = ["accepted" , "declined" , "tentative" , "needs-action"] as const
+export const ActorStatusArray = ["accepted", "declined", "tentative", "needs-action"] as const
 type ActorStatus = typeof ActorStatusArray[number]
 
-export interface APPOINTMENT{
-    id?:string;
-    status : AppointmentStatus
-    patientId: string;
-    practitionerId: string;
-    text :string
-    serviceCategory:CodeDisplay[]
-    serviceType : CodeDisplay[]
-    appointmentType : CodeDisplay[]
-    reasonReferenceConditionId : string;
-    createdDate:string;
-    startDate:string;
-    endDate:string
-    description:string
-    patientStatus:ActorStatus
-    practitionerStatus :ActorStatus
+export interface APPOINTMENT {
+  id?: string;
+  status: AppointmentStatus
+  patientId: string;
+  practitionerId: string;
+  text: string
+  serviceCategory: CodeDisplay[]
+  serviceType: CodeDisplay[]
+  appointmentType: CodeDisplay[]
+  reasonReferenceConditionId: string;
+  createdDate: string;
+  startDate: string;
+  endDate: string
+  description: string
+  patientStatus: ActorStatus
+  practitionerStatus: ActorStatus
 }
 
 export class Appointment implements ResourceMaster {
-  getFHIR(options: APPOINTMENT):any {
+  getFHIR(options: APPOINTMENT): any {
     const body = {
       resourceType: "Appointment",
       id: options.id || undefined,
@@ -52,7 +52,7 @@ export class Appointment implements ResourceMaster {
         },
       ],
       appointmentType: {
-        coding:options.appointmentType,
+        coding: options.appointmentType,
       },
       reasonReference: [
         {
@@ -82,7 +82,25 @@ export class Appointment implements ResourceMaster {
 
     return body
   }
-  convertFhirToObject(options: any) {
-    throw new Error("Method not implemented.");
+  convertFhirToObject(options: any): APPOINTMENT {
+    let ret: APPOINTMENT = {
+      status: options.status,
+      patientId: `${options.participant[0].actor.reference}`.substring(8),
+      practitionerId: `${options.participant[1].actor.reference}`.substring(13),
+      text: options.text,
+      serviceCategory: options.serviceCategory,
+      serviceType: options.serviceType,
+      appointmentType: options.appointmentType,
+      reasonReferenceConditionId: `${options.reasonReference.reference}`.substring(8),
+      createdDate: options.created,
+      startDate: options.start,
+      endDate: options.end,
+      description: options.description,
+      patientStatus: options.participant[0].status,
+      practitionerStatus: options.participant[0].status,
+      id: options.id
+    }
+
+    return ret;
   }
 }
