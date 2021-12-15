89469eb070a7feacde63526180f7b13020427104
pradyumnabilagi <pradyumnabilagi12@gmail.com>
2021-12-15 16:16:09 +0530

    done

--- src/resources/Practitioner.ts
+++ src/resources/Practitioner.ts
@@ -1,12 +1,73 @@
+import { ResourceMaster } from "../Interfaces";
 
 interface PRACTITIONER {
   id?: string
   name: string
-  qualification: string
+  qualification?: string
   medicalLicenseNumber: string
   ndhmProfessionalId: string
 
 }
+
+export class Practitioner implements ResourceMaster{
+  getFHIR(options: PRACTITIONER) {
+    const body = {
+      "resourceType": "Practitioner",
+      "id": `${options.id}`,
+      "meta": {
+        "versionId": "1",
+        "lastUpdated": new Date().toDateString(),
+        "profile": [
+          "https://nrces.in/ndhm/fhir/r4/StructureDefinition/Practitioner"
+        ]
+      },
+      "text": {
+        "status": "generated",
+        "div": `<div xmlns=\"http://www.w3.org/1999/xhtml\">${options.name}, ${options.qualification})</div>`
+      },
+      "identifier": [
+        {
+          "type": {
+            "coding": [
+              {
+                "system": "http://terminology.hl7.org/CodeSystem/v2-0203",
+                "code": "MD",
+                "display": `${options.medicalLicenseNumber}`
+              }
+            ]
+          },
+          "system": "https://doctor.ndhm.gov.in",
+          "value": `${options.ndhmProfessionalId}`
+        }
+      ],
+      "name": [
+        {
+          "text": `${options.name} ${options.qualification}`
+        }
+      ]
+    }
+  
+  
+    return body;
+  }
+  convertFhirToObject(options: any):PRACTITIONER {
+    let ret:PRACTITIONER={
+      name: options.name[0].text,
+      medicalLicenseNumber:  options.identifier[0].type.coding[0].display,
+      ndhmProfessionalId: options.identifier[0].value,
+      id:options.id
+    }
+    return ret;
+  }
+
+}
+
+
+/**
+* @deprecated
+ * @param options 
+ * @returns 
+ */
 const PractitionerResource = (options: PRACTITIONER) => {
 
 
