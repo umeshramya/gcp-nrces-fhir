4bb752c39d3696bf8c65bd8babfaf5c026e88364
pradyumnabilagi <pradyumnabilagi12@gmail.com>
2021-12-18 18:26:56 +0530

    done

---
+++ src/resources/observations/DocumentReference.ts
@@ -0,0 +1,69 @@
+import { ResourceMaster } from "../../Interfaces";
+
+export const documentStatusArrey = ["current" , "superseded" , "entered-in-error"
+] as const
+type DocumentStatus = typeof documentStatusArrey[number]
+
+export const documentDocStatusArrey = [	"preliminary" , "final" , "amended" , "entered-in-error"
+] as const
+type DocumentDocStatus = typeof documentDocStatusArrey[number]
+
+export interface DOCUMENT_REFERENCE{
+    id?:string;
+    status : DocumentStatus;
+    docStatus:DocumentDocStatus
+
+}
+
+
+export class DocumentReference implements ResourceMaster{
+    getFHIR(options: DOCUMENT_REFERENCE) {
+        const body = {
+            resourceType: "DocumentReference",
+            id: options.id || undefined,
+            meta: {
+              profile: [
+                "httcps://nrces.in/ndhm/fhir/r4/StructureDefinition/DocumentReference",
+              ],
+            },
+            text: {
+              status: "generated",
+              div: '<div xmlns="http://www.w3.org/1999/xhtml"><p><b>Narrative with Details</b></p><p><b>id</b>: 1</p><p><b>status</b>: current</p><p><b>docStatus</b>: final</p><p><b>subject</b>: ABC</p><p><b>type</b>: Laboratory Report</p><p><b>content</b>: Dr. PQR</p></div>',
+            },
+            status: options.status,
+            docStatus: options.docStatus,
+            type: {
+              coding: [
+                {
+                  system: "http://snomed.info/sct",
+                  code: "4241000179101",
+                  display: "Laboratory report",
+                },
+              ],
+              text: "Laboratory report",
+            },
+            subject: { reference: "Patient/1" },
+            content: [
+              {
+                attachment: {
+                  contentType: "application/pdf",
+                  language: "en-IN",
+                  data: "",
+                  title: "Laboratory report",
+                  creation: "2019-05-29T14:58:58.181+05:30",
+                },
+              },
+            ],
+          };
+
+
+          return body;
+    }
+    convertFhirToObject(options: any) {
+        throw new Error("Method not implemented.");
+    }
+    
+}
+
+
+
