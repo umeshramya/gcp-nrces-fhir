0798a519901387901a202171ac9d9ca27b03a29f
pradyumnabilagi <pradyumnabilagi12@gmail.com>
2022-03-13 12:06:17 +0530

    done

--- src/resources/Encounter.ts
+++ src/resources/Encounter.ts
@@ -1,7 +1,18 @@
-import { ResourceMaster } from "../Interfaces"
-import ResourceMain from "./ResourceMai"
+import { IDENTTIFIER } from "../config";
+import { ResourceMaster } from "../Interfaces";
+import ResourceMain from "./ResourceMai";
 
-const EncounterStatusArray = ["planned", "arrived", "triaged", "in-progress", "onleave", "finished", "cancelled", "entered-in-error", "unknown"] as const
+const EncounterStatusArray = [
+  "planned",
+  "arrived",
+  "triaged",
+  "in-progress",
+  "onleave",
+  "finished",
+  "cancelled",
+  "entered-in-error",
+  "unknown",
+] as const;
 const EncounterClassArray = [
   { code: "AMB", display: "ambulatory" },
   { code: "FLD", display: "Field" },
@@ -13,7 +24,7 @@ const EncounterClassArray = [
   { code: "OBSENC", display: "observation encounter" },
   { code: "PRENC", display: "pre-admission" },
   { code: "VR", display: "virtual" },
-] as const
+] as const;
 
 const EncounterHospitalizationDischargeDispositionArray = [
   { code: "home", display: "home" },
@@ -27,171 +38,144 @@ const EncounterHospitalizationDischargeDispositionArray = [
   { code: "rehab", display: "Rehabilitation" },
   { code: "smf", display: "Skilled nursing facility" },
   { code: "oth", display: "Other" },
-] as const
+] as const;
 
-type EncounterStatus = typeof EncounterStatusArray[number]
-type EncounterClass = typeof EncounterClassArray[number]
-type EncounterHospitalizationDischargeDisposition = typeof EncounterHospitalizationDischargeDispositionArray[number]
-
+type EncounterStatus = typeof EncounterStatusArray[number];
+type EncounterClass = typeof EncounterClassArray[number];
+type EncounterHospitalizationDischargeDisposition =
+  typeof EncounterHospitalizationDischargeDispositionArray[number];
 
 interface ENCOUNTER {
-  id?: string
-  text: string
-  status: EncounterStatus
-  identifier: string
-  class: EncounterClass
-  patientId: string
-  startDate: string
-  endDate: string
-  dischargeDisposition: EncounterHospitalizationDischargeDisposition
-
+  id?: string;
+  text: string;
+  status: EncounterStatus;
+  careContext?: string;
+  class: EncounterClass;
+  patientId: string;
+  startDate: string;
+  endDate: string;
+  dischargeDisposition: EncounterHospitalizationDischargeDisposition;
 }
 
 export class Encounter extends ResourceMain implements ResourceMaster {
-
   getFHIR(options: ENCOUNTER) {
+    const identifiers: IDENTTIFIER[] = [];
 
-    const body = {
-      "resourceType": "Encounter",
-      "id": options.id,
-      "meta": {
-        "lastUpdated": new Date().toISOString(),
-        "profile": [
-          "https://nrces.in/ndhm/fhir/r4/StructureDefinition/Encounter"
-        ]
-      },
-      "text": {
-        "status": "generated",
-        "div": `<div xmlns=\"http://www.w3.org/1999/xhtml\">${options.text} </div>`
-      },
-      "identifier": [
-        {
-          "system": "https://ndhm.in",
-          "value": options.identifier
-        }
-      ],
-      "status": options.status,
-      "class": {
-        "system": "http://terminology.hl7.org/CodeSystem/v3-ActCode",
-        "code": options.class.code,
-        "display": options.class.display
-      },
-      "subject": {
-        "reference": `Patient/${options.patientId}`
-      },
-      "period": {
-        "start": options.startDate,
-        "end": options.endDate
-      },
-      "hospitalization": {
-        "dischargeDisposition": {
-          "coding": [
+    if (options.careContext) {
+      const id: IDENTTIFIER = {
+        type: {
+          coding: [
             {
-              "system": "http://terminology.hl7.org/CodeSystem/discharge-disposition",
-              "code": options.dischargeDisposition.code,
-              "display": options.dischargeDisposition.display
-            }
+              system: "http://terminology.hl7.org/CodeSystem/v2-0203",
+              code: "MR",
+              display: "Medical record number",
+            },
           ],
-          "text": "Discharged to Home Care"
-        }
-      }
+        },
+        system: "https://ndhm.in",
+        value: `${options.careContext}`,
+      };
+
+      identifiers.push(id);
     }
 
-    return body
+    const body = {
+      resourceType: "Encounter",
+      id: options.id,
+      meta: {
+        lastUpdated: new Date().toISOString(),
+        profile: [
+          "https://nrces.in/ndhm/fhir/r4/StructureDefinition/Encounter",
+        ],
+      },
+      text: {
+        status: "generated",
+        div: `<div xmlns=\"http://www.w3.org/1999/xhtml\">${options.text} </div>`,
+      },
+      identifier: identifiers,
+      // [
+      //   {
+      //     system: "https://ndhm.in",
+      //     value: options.careContext,
+      //   },
+      // ],
+      status: options.status,
+      class: {
+        system: "http://terminology.hl7.org/CodeSystem/v3-ActCode",
+        code: options.class.code,
+        display: options.class.display,
+      },
+      subject: {
+        reference: `Patient/${options.patientId}`,
+      },
+      period: {
+        start: options.startDate,
+        end: options.endDate,
+      },
+      hospitalization: {
+        dischargeDisposition: {
+          coding: [
+            {
+              system:
+                "http://terminology.hl7.org/CodeSystem/discharge-disposition",
+              code: options.dischargeDisposition.code,
+              display: options.dischargeDisposition.display,
+            },
+          ],
+          text: "Discharged to Home Care",
+        },
+      },
+    };
+
+    return body;
   }
-
-
 
   convertFhirToObject(options: any): ENCOUNTER {
     let ret: ENCOUNTER = {
       text: this.getDivText(options.text.div),
       status: options.status,
-      identifier: options.identifier[0].value,
-      class: { "code": options.class.code, "display": options.class.display },
+      class: { code: options.class.code, display: options.class.display },
       patientId: `${options.subject.reference}`.substring(8),
       startDate: options.period.start,
       endDate: options.period.end,
-      dischargeDisposition: { "code": options.hospitalization.dischargeDisposition.coding[0].code, "display": options.hospitalization.dischargeDisposition.coding[0].display },
-      id: options.id
+      dischargeDisposition: {
+        code: options.hospitalization.dischargeDisposition.coding[0].code,
+        display: options.hospitalization.dischargeDisposition.coding[0].display,
+      },
+      id: options.id,
+    };
 
+    const careContext: any[] = options.identifier.filter(
+      (el: any) => el.system == "https://ndhm.in"
+    );
+
+    if (careContext.length > 0) {
+      ret.careContext = careContext[0].value;
     }
 
     return ret;
   }
-
-
 }
-
 
 /**
  * @deprecated
- * @param options 
- * @returns 
+ * @param options
+ * @returns
  */
 const EncounterResource = (options: ENCOUNTER) => {
   const encounter = new Encounter();
-  return encounter.getFHIR(options)
-}
+  return encounter.getFHIR(options);
+};
 
-export { ENCOUNTER, EncounterResource, EncounterHospitalizationDischargeDispositionArray, EncounterStatusArray, EncounterClassArray }
-export type { EncounterClass, EncounterStatus, EncounterHospitalizationDischargeDisposition }
-
-
-
-
-
-
-// const body1 = {
-//   "resourceType": "Encounter",
-//   // "id": "example-01",
-//   "meta": {
-//     "lastUpdated": "2020-07-09T14:58:58.181+05:30",
-//     "profile": [
-//       "https://nrces.in/ndhm/fhir/r4/StructureDefinition/Encounter"
-//     ]
-//   },
-//   "text": {
-//     "status": "generated",
-//     "div": "<div xmlns=\"http://www.w3.org/1999/xhtml\"> Admitted to Cardiac Unit,UVW Hospital between June 28 and July 9 2020</div>"
-//   },
-//   "identifier": [
-//     {
-//       "system": "https://ndhm.in",
-//       "value": "S100"
-//     }
-//   ],
-//   "status": "finished",
-//   "class": {
-//     "system": "http://terminology.hl7.org/CodeSystem/v3-ActCode",
-//     "code": "IMP",
-//     "display": "inpatient encounter"
-//   },
-//   "subject": {
-//     "reference": "Patient/8c2f7c57-cfba-417c-a574-36c6e76d29c5"
-//   },
-//   "period": {
-//     "start": "2020-04-20T15:32:26.605+05:30",
-//     "end": "2020-05-01T15:32:26.605+05:30"
-//   },
-//   "hospitalization": {
-//     "dischargeDisposition": {
-//       "coding": [
-//         {
-//           "system": "http://terminology.hl7.org/CodeSystem/discharge-disposition",
-//           "code": "home",
-//           "display": "Home"
-//         }
-//       ],
-//       "text": "Discharged to Home Care"
-//     }
-//   }
-// }
-
-
-
-
-
-
-
-
-
+export {
+  ENCOUNTER,
+  EncounterResource,
+  EncounterHospitalizationDischargeDispositionArray,
+  EncounterStatusArray,
+  EncounterClassArray,
+};
+export type {
+  EncounterClass,
+  EncounterStatus,
+  EncounterHospitalizationDischargeDisposition,
+};
