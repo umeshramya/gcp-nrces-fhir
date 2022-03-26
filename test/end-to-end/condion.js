1d2588009374abf759a7a959a945d786db9f51d3
pradyumnabilagi <pradyumnabilagi12@gmail.com>
2022-03-26 15:59:14 +0530

    done

---
+++ test/end-to-end/condion.js
@@ -0,0 +1,25 @@
+require('dotenv').config("env")
+const v4 = require("uuid").v4
+const { cpSync } = require('fs')
+const { GcpFhirCRUD, GcpFhirSearch, Encounter, OrganizationResource, PatientResource, Patient, PractitionerResource, EncounterResource, EncounterClassArray, EncounterStatusArray, Procedure, Condition, AllergyIntolerance, Appointment, DocumentBundle, Composition, Organization, Practitioner, MedicationRequest, PrescriptionRecord, OPConsultRecord, ResourceFactory, PrescriptionBundle } = require("gcp-nrces-fhir")
+const { emptySign } = require('gcp-nrces-fhir/lib/resources/Composition');
+const gcpFhirCRUD = new GcpFhirCRUD();
+const { callFunction, resources } = require("./index")
+
+
+const setCondition = async () => {
+const condition = new Condition();
+let res = condition.getFHIR({
+    "condtion" : [{"display" :"Chest Pain", "system" : "http://snomed.info/sct"}],
+    "patientId" : resources.patient.id,
+    "text" : "Chest pain for evaluvation",
+    "title":  "Chest pain",
+})
+    res = await gcpFhirCRUD.createFhirResource(res, "Condition")
+    resources.conditon = condition.convertFhirToObject(res.data);
+  
+  
+  }
+
+
+module.exports= {setCondition}
--- test/end-to-end/excute.js
+++ test/end-to-end/excute.js
@@ -1,14 +1,21 @@
 const { setMedicationRequest } = require("./medication")
-const { callFunction } = require("./index")
+const { callFunction } = require("./index");
+const { setCondition } = require("./condion");
 class excute {
 
     medicationrequest = async () => {
         await callFunction();
         await setMedicationRequest();
     }
+
+    conditon = async ()=>{
+        await callFunction();
+        await setCondition();
+    }
 }
 
 
 
 
-new excute().medicationrequest();
+// new excute().medicationrequest();
+new excute().conditon()
