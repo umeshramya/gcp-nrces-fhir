7819419ca69efaf705d6af00e9acf9e7a8be884d
unknown <pradyumnabilagi12@gmail.com>
2022-04-05 08:23:07 +0530

    done

--- src/resources/Composition/DiagnosticReportComp.ts
+++ src/resources/Composition/DiagnosticReportComp.ts
@@ -1,7 +1,24 @@
 import { Composition, COMPOSITOIN, Records } from ".";
 import GcpFhirCRUD from "../../classess/gcp";
 
+interface args{ 
+  composition: COMPOSITOIN;
+  diagnosticReport:any;
+  requester:any;
+  performer?:any;
+  media:any
+}
+
 export class DiagnosticReportComp extends Composition implements Records {
-  create = async (options: { composition: COMPOSITOIN }) => {};
+  create = async (options: args) => {
+    this.setRequester(options.requester)
+    if(options.performer){
+      this.setPerformer(options.performer)
+    }
+    this.setPerformer(options.performer)
+
+    
+
+  };
   update = async (options: {}) => {};
 }
