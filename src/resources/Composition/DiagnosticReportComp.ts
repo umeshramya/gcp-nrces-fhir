0494129fec616cf7cd7f8fb54114166b5e192b87
umeshramya <umeshbilagi@gmail.com>
2022-04-08 01:33:06 +0530

    done

--- src/resources/Composition/DiagnosticReportComp.ts
+++ src/resources/Composition/DiagnosticReportComp.ts
@@ -1,11 +1,11 @@
 import { Composition, COMPOSITOIN, Records } from ".";
 import GcpFhirCRUD from "../../classess/gcp";
+import { DiagnosticReport } from "../DiagnosticReport";
+import { ServiceRequest } from "../ServiceRequest";
 
 interface args {
   composition: COMPOSITOIN;
   diagnosticReport: any;
-  requester: any;
-  performer?: any;
   media: []
   serviceRequest: any
 }
@@ -17,29 +17,38 @@ export class DiagnosticReportComp extends Composition implements Records {
     //   this.setPerformer(options.performer)
     // }
     // this.setPerformer(options.performer)
-  
+    const serviceRequest = new ServiceRequest();
+    const serviceRequestBody = serviceRequest.convertFhirToObject(serviceRequest)
+    this.setRequester({
+      "reesource": serviceRequestBody.requester.resource,
+      "display": serviceRequestBody.requester.display
+    })
+
+    const diagnosticReport = new DiagnosticReport();
+    const diagnosticReportObj = diagnosticReport.convertFhirToObject(diagnosticReport);
+
 
     options.composition.section.push(
-        {
-            "title": "Computed tomography imaging report",
-            "code": {
-                "coding": [
-                    {
-                        "system": "http://snomed.info/sct",
-                        "code": "4261000179100",
-                        "display": "Computed tomography imaging report"
-                    }
-                ]
-            },
-            "entry": [
-              {
-                reference: `DiagnosticReport/${options.diagnosticReport.id}`,
-                type: "DiagnosticReport",
-              }
-            ]
-        }
+      {
+        "title": "Computed tomography imaging report",
+        "code": {
+          "coding": [
+            {
+              "system": "http://snomed.info/sct",
+              "code": "4261000179100",
+              "display": "Computed tomography imaging report"
+            }
+          ]
+        },
+        "entry": [
+          {
+            reference: `DiagnosticReport/${options.diagnosticReport.id}`,
+            type: "DiagnosticReport",
+          }
+        ]
+      }
     );
-      
+
     options.composition.documentDatahtml = options.diagnosticReport.text.div;
 
 
