d90e6d40d03e0b9dc02b22bc76f4263b14756edf
pradyumnabilagi <pradyumnabilagi12@gmail.com>
2022-04-09 07:52:02 +0530

    done

--- src/resources/Composition/DiagnosticReportComp.ts
+++ src/resources/Composition/DiagnosticReportComp.ts
@@ -6,58 +6,48 @@ import { ServiceRequest } from "../ServiceRequest";
 interface args {
   composition: COMPOSITOIN;
   diagnosticReport: any;
-  media: []
-  serviceRequest: any
+  media: [];
+  serviceRequest: any;
 }
 
 export class DiagnosticReportComp extends Composition implements Records {
-  create = async (options: args) => {
-
-
+  private setPerformerAndRequester = (options: args) => {
     const serviceRequest = new ServiceRequest();
-    const serviceRequestBody = serviceRequest.convertFhirToObject(options.serviceRequest)
-
-    const diagnosticReport = new DiagnosticReport();
-    const diagnosticReportObj = diagnosticReport.convertFhirToObject(options.diagnosticReport);
-
-
-
-    this.setRequester({
-      "reesource": serviceRequestBody.requester.resource,
-      "display": serviceRequestBody.requester.display
-    })
-
-    diagnosticReportObj.performer.forEach(el => {
-      if (el.display) {
-        this.setPerformer({ "display": el.display, "reesource": el.resource })
-      }
-
-    })
-
-
-    options.composition.section.push(
-      {
-        "title": "Computed tomography imaging report",
-        "code": {
-          "coding": [
-            {
-              "system": "http://snomed.info/sct",
-              "code": "4261000179100",
-              "display": "Computed tomography imaging report"
-            }
-          ]
-        },
-        "entry": [
-          {
-            reference: `DiagnosticReport/${options.diagnosticReport.id}`,
-            type: "DiagnosticReport",
-          }
-        ]
-      }
+    const serviceRequestBody = serviceRequest.convertFhirToObject(
+      options.serviceRequest
     );
 
-    options.composition.documentDatahtml = options.diagnosticReport.text.div;
+    const diagnosticReport = new DiagnosticReport();
+    const diagnosticReportObj = diagnosticReport.convertFhirToObject(
+      options.diagnosticReport
+    );
 
+    this.setRequester({
+      reesource: serviceRequestBody.requester.resource,
+      display: serviceRequestBody.requester.display,
+    });
+
+    diagnosticReportObj.performer.forEach((el) => {
+      if (el.display) {
+        this.setPerformer({ display: el.display, reesource: el.resource });
+      }
+    });
+  };
+  create = async (options: args) => {
+    this.setPerformerAndRequester(options);
+
+    options.composition.section.push({
+      title: "Computed tomography imaging report",
+      code: options.diagnosticReport.code,
+      entry: [
+        {
+          reference: `DiagnosticReport/${options.diagnosticReport.id}`,
+          type: "DiagnosticReport",
+        },
+      ],
+    });
+
+    options.composition.documentDatahtml = options.diagnosticReport.text.div;
 
     const body = this.getFHIR(options.composition);
 
@@ -66,5 +56,31 @@ export class DiagnosticReportComp extends Composition implements Records {
     const res = await gcpFhirCrud.createFhirResource(body, "Composition");
     return res;
   };
-  update = async (options: {}) => { };
+  update = async (options: args) => {
+    this.setPerformerAndRequester(options);
+    options.composition.section = [];
+    options.composition.section.push({
+      title: "Computed tomography imaging report",
+      code: options.diagnosticReport.code,
+      entry: [
+        {
+          reference: `DiagnosticReport/${options.diagnosticReport.id}`,
+          type: "DiagnosticReport",
+        },
+      ],
+    });
+
+    options.composition.documentDatahtml = options.diagnosticReport.text.div;
+
+    const body = this.getFHIR(options.composition);
+
+    body.section = options.composition.section;
+    const gcpFhirCrud = new GcpFhirCRUD();
+    const res = await gcpFhirCrud.updateFhirResource(
+      body,
+      options.composition.id || "",
+      "Composition"
+    );
+    return res;
+  };
 }
