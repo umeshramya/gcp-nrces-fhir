1c37637213d57981f6a8e1d3e1917df8e4fb5a61
unknown <umeshbilagi@gmail.com>
2022-07-09 01:35:40 +0530

    done

---
+++ src/resources/CarePlan.ts
@@ -0,0 +1,63 @@
+import { CODEABLE_CONCEPT } from "../config";
+import { ResourceMaster } from "../Interfaces";
+import { PATIENT } from "./Patient";
+import ResourceMain from "./ResourceMai";
+
+
+ const carePlanStatusArray = ["draft" , "active" , "on-hold" , "revoked" , "completed" , "entered-in-error" , "unknown"]as const
+export type CarePlanStatus = typeof carePlanStatusArray;
+
+export const carePlanIntentArray = ["proposal" , "plan" ,"order" , "option"]as const
+export type CarePlanIntent = typeof carePlanIntentArray
+
+
+export interface CARE_PLAN{
+    id?:string
+    status:CarePlanStatus
+    inetent: CarePlanIntent
+    text : string
+    category?:CODEABLE_CONCEPT[]
+    title?:string
+    description?:string
+    patient:PATIENT;
+
+}
+export class CarePlan extends ResourceMain implements ResourceMaster {
+  getFHIR(options: CARE_PLAN) {
+    const body = {
+      resourceType: "CarePlan",
+      id: options.id || undefined,
+      meta: {
+        profile: ["https://nrces.in/ndhm/fhir/r4/StructureDefinition/CarePlan"],
+      },
+      text: {
+        status: "additional",
+        div: options.text,
+      },
+      status: options.status,
+      intent: options.inetent,
+      category: options.category,
+      title: options.title,
+      description: options.description,
+      subject: {
+        reference: `Patient/${options.patient.id}`,
+        display: options.patient.name,
+      },
+      activity: [
+        {
+          outcomeReference: [
+            {
+              reference: "Appointment/1",
+            },
+          ],
+        },
+      ],
+    };
+  }
+  convertFhirToObject(options: any) {
+    throw new Error("Method not implemented.");
+  }
+  statusArray = ():CarePlanStatus[]=>{
+    return carePlanStatusArray.map(el=>el) as any
+  }
+}
