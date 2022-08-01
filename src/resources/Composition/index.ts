1aa974ef6b2915cb2e50fdea0c552779503770aa
umeshramya <umeshbilagi@gmail.com>
2022-08-01 10:23:23 +0530

    done

--- src/resources/Composition/index.ts
+++ src/resources/Composition/index.ts
@@ -14,7 +14,7 @@ import { PDF_FOOter } from "js-ts-report/build/classes/create-pdf";
 export const compositionTypeArrey = [
   {
     type: "OPConsultation",
-    system: "https://projecteka.in/sct",
+    system: "https://ndhm.gov.in/sct",
     url: "https://nrces.in/ndhm/fhir/r4/StructureDefinition/OPConsultRecord",
     code: "371530004",
     text: "Clinical consultation report",
@@ -22,7 +22,7 @@ export const compositionTypeArrey = [
 
   {
     type: "DischargeSummary",
-    system: "https://projecteka.in/sct",
+    system: "https://ndhm.gov.in/sct",
     url: "https://nrces.in/ndhm/fhir/r4/StructureDefinition/DischargeSummaryRecord",
     code: "373942005",
     text: "Discharge summary",
@@ -30,21 +30,21 @@ export const compositionTypeArrey = [
 
   {
     type: "ImmunizationRecord",
-    system: "https://projecteka.in/sct",
+    system: "https://ndhm.gov.in/sct",
     url: "https://nrces.in/ndhm/fhir/r4/StructureDefinition/ImmunizationRecord",
     code: "41000179103",
     text: "Immunization record",
   },
   {
     type: "Prescription",
-    system: "https://projecteka.in/sct",
+    system: "https://ndhm.gov.in/sct",
     url: "https://nrces.in/ndhm/fhir/r4/StructureDefinition/PrescriptionRecord",
     code: "440545006",
     text: "Prescription record",
   },
   {
     type: "DiagnosticReport",
-    system: "https://projecteka.in/sct",
+    system: "https://ndhm.gov.in/sct",
     url: "https://nrces.in/ndhm/fhir/r4/StructureDefinition/DiagnosticReportRecord",
     code: "721981007",
     text: "Diagnostic studies report",
@@ -97,7 +97,7 @@ export interface USER_COMPOSITION_EXTENSION {
 export class Composition extends ResourceMain implements ResourceMaster {
   private compType!: {
     type: string;
-    system: "https://projecteka.in/sct";
+    system: "https://ndhm.gov.in/sct";
     url: string;
     code: string;
     text: string;
@@ -185,11 +185,11 @@ export class Composition extends ResourceMain implements ResourceMaster {
       let ret = `<div>Patient:- ${options.patient.name}.</div>`;
       ret += `<div>MRN:- ${options.patient.MRN} </div>`;
       ret += `${options.patient.phrAddress
-          ? `<div>ABHA Address : ${options.patient.phrAddress}. ${options.patient.healthNumber
-            ? `ABHA Number ${options.patient.healthNumber}`
-            : ""
-          }</div>`
+        ? `<div>ABHA Address : ${options.patient.phrAddress}. ${options.patient.healthNumber
+          ? `ABHA Number ${options.patient.healthNumber}`
           : ""
+        }</div>`
+        : ""
         }`;
       ret += `<div>Gender/Age: ${options.patient.gender}/${new Age().dobToAge(
         new Date(options.patient.dob)
@@ -230,21 +230,21 @@ export class Composition extends ResourceMain implements ResourceMaster {
       html += `<td>${getDoctors()}`;
 
       html += `${this.performer.length > 0
-          ? `<div>Performed By :${this.performer.reduce(
-            (pr, cu) => (pr += `${cu}<\br>`)
-          )}</div>`
-          : ""
+        ? `<div>Performed By :${this.performer.reduce(
+          (pr, cu) => (pr += `${cu}<\br>`)
+        )}</div>`
+        : ""
         }`;
       html += `</td>`;
       html += `</tr>`;
 
       html += `${this.requeter || options.patient.internalId
-          ? `<tr><td>${this.requeter ? `Requested By : ${this.requeter}` : ""
-          }</td><td>${options.patient.internalId
-            ? `Internal Id : ${options.patient.internalId}`
-            : ""
-          }</td></tr>`
+        ? `<tr><td>${this.requeter ? `Requested By : ${this.requeter}` : ""
+        }</td><td>${options.patient.internalId
+          ? `Internal Id : ${options.patient.internalId}`
           : ""
+        }</td></tr>`
+        : ""
         }`;
 
       html += `</table>`;
@@ -288,7 +288,7 @@ export class Composition extends ResourceMain implements ResourceMaster {
       type: {
         coding: [
           {
-            system: "https://projecteka.in/sct",
+            system: "https://ndhm.gov.in/sct",
             code: this.compType.code,
             display: this.compType.text,
           },
@@ -315,7 +315,7 @@ export class Composition extends ResourceMain implements ResourceMaster {
           code: {
             coding: [
               {
-                system: "https://projecteka.in/sct",
+                system: "https://ndhm.gov.in/sct",
                 code: this.compType.code,
                 display: options.type,
               },
