a30c51a8037c09636e4b4934d502bf817fee9312
unknown <umeshbilagi@gmail.com>
2022-09-15 19:39:22 +0530

    done

--- src/resources/Bundle/Prescription.ts
+++ src/resources/Bundle/Prescription.ts
@@ -7,6 +7,103 @@ import { MedicationRequest } from "../MedicationRequest";
 import { Condition } from "../Condition";
 
 export class PrescriptionBundle extends BundelMain implements ResourceMaster {
+  // async getFHIR(options: {
+  //   id?: string;
+  //   identifier?: IDENTTIFIER;
+  //   composition: any;
+  //   pdfData: string;
+  // }) {
+  //   if (options.identifier) {
+  //     let ret: IDENTTIFIER = {
+  //       system: "http://www.nicehms.com/bundle",
+  //       value: options.identifier.value,
+  //     };
+  //   }
+
+  //   const bundlemain = await new BundelMain(this.gcpCredetials, this.gcpPath).getentries(
+  //     options.composition,
+  //     options.pdfData
+  //   );
+
+  //   const entry = bundlemain.entry;
+   
+  //   const sectionEntries = bundlemain.compositionObj.section[0].entry as {
+  //     reference: string;
+  //     type: resourceType;
+  //   }[];
+
+  //    // write code to pusj medication trequest here
+  //   const medicationRequestId = this.getIdFromReference({
+  //     ref: sectionEntries.filter((el) => el.type == "MedicationRequest")[0]
+  //       .reference,
+  //     resourceType: "MedicationRequest",
+  //   });
+
+  //   const gcpFhirCrud = new GcpFhirCrud(this.gcpCredetials, this.gcpPath)
+  //   const medicationRequest = new MedicationRequest().bundlify(await gcpFhirCrud
+  //     .getFhirResource(medicationRequestId, "MedicationRequest")
+  //     .then((res) => res.data));
+  //     medicationRequest.forEach((el:any, i:number)=>{
+  //      entry.push(el)
+  //       if(i!=0){
+  //         entry[0].resource.section[0].entry.push( {
+  //           "reference": el.fullUrl
+  //         })
+  //       }
+  //     })
+
+  //   // Get Condition
+  //   const conditionArray = sectionEntries.filter(el=> el.type == "Condition")
+  //   if(conditionArray.length >0){
+  //     const conditionId = this.getIdFromReference({
+  //       "ref" : conditionArray[0].reference,
+  //       resourceType : "Condition"
+  //     })
+
+      
+  //     const condition = await gcpFhirCrud.getFhirResource(conditionId, "Condition").then(res=>res.data)
+  //     entry.push({
+  //       fullUrl: `Condition/${conditionId}`,
+  //       resource: new Condition().bundlify(condition),
+  //     })
+  //   }
+
+  //   // Binary
+  //   const binaryArray = sectionEntries.filter(el=> el.type == "Binary")
+  //   if(binaryArray.length > 0){
+  //     const binaryId = this.getIdFromReference({
+  //       "ref" : binaryArray[0].reference,
+  //       "resourceType" : "Binary"
+  //     })
+  //     const binary = await gcpFhirCrud.getFhirResource(binaryId, "Binary").then(res=>res.data);
+  //     entry.push({
+  //       fullUrl: `Binary/${binaryId}`,
+  //       resource: binary,
+  //     })
+  //   }
+
+
+  //  const  filteredEntry = entry.filter(el =>el.resource.resourceType !== "DocumentReference")
+
+ 
+
+  //   const body = {
+  //     "resourceType": "Bundle",
+  //     "id": options.id,
+  //     "meta": {
+  //       "lastUpdated": new Date().toISOString()
+  //     },
+  //     "identifier": {
+  //       "system": "https://www.nicehms.com/bundle",
+  //       "value": options.id
+  //     },
+  //     "type": "document",
+  //     "timestamp": options.composition.date,
+  //     "entry": filteredEntry
+  //   }
+  //   return body;
+  // }
+
   async getFHIR(options: {
     id?: string;
     identifier?: IDENTTIFIER;
@@ -15,93 +112,59 @@ export class PrescriptionBundle extends BundelMain implements ResourceMaster {
   }) {
     if (options.identifier) {
       let ret: IDENTTIFIER = {
-        system: "http://www.nicehms.com/bundle",
+        system: "http://www.nicehms.com",
         value: options.identifier.value,
       };
     }
+    // options.composition.title = "OP Consultation Document";
 
-    const bundlemain = await new BundelMain(this.gcpCredetials, this.gcpPath).getentries(
-      options.composition,
-      options.pdfData
-    );
+    const bundlemain = await new BundelMain(
+      this.gcpCredetials,
+      this.gcpPath
+    ).getentries(options.composition, options.pdfData);
 
-    const entry = bundlemain.entry;
-   
-    const sectionEntries = bundlemain.compositionObj.section[0].entry as {
-      reference: string;
-      type: resourceType;
-    }[];
+    this.entry = bundlemain.entry;
 
-     // write code to pusj medication trequest here
-    const medicationRequestId = this.getIdFromReference({
-      ref: sectionEntries.filter((el) => el.type == "MedicationRequest")[0]
-        .reference,
-      resourceType: "MedicationRequest",
-    });
+    const sectionEntries = options.composition.section;
 
-    const gcpFhirCrud = new GcpFhirCrud(this.gcpCredetials, this.gcpPath)
-    const medicationRequest = new MedicationRequest().bundlify(await gcpFhirCrud
-      .getFhirResource(medicationRequestId, "MedicationRequest")
-      .then((res) => res.data));
-      medicationRequest.forEach((el:any, i:number)=>{
-       entry.push(el)
-        if(i!=0){
-          entry[0].resource.section[0].entry.push( {
-            "reference": el.fullUrl
-          })
-        }
-      })
-
-    // Get Condition
-    const conditionArray = sectionEntries.filter(el=> el.type == "Condition")
-    if(conditionArray.length >0){
-      const conditionId = this.getIdFromReference({
-        "ref" : conditionArray[0].reference,
-        resourceType : "Condition"
-      })
-
-      
-      const condition = await gcpFhirCrud.getFhirResource(conditionId, "Condition").then(res=>res.data)
-      entry.push({
-        fullUrl: `Condition/${conditionId}`,
-        resource: new Condition().bundlify(condition),
-      })
-    }
-
-
-    // Binary
-    const binaryArray = sectionEntries.filter(el=> el.type == "Binary")
-    if(binaryArray.length > 0){
-      const binaryId = this.getIdFromReference({
-        "ref" : binaryArray[0].reference,
-        "resourceType" : "Binary"
-      })
-      const binary = await gcpFhirCrud.getFhirResource(binaryId, "Binary").then(res=>res.data);
-      entry.push({
-        fullUrl: `Binary/${binaryId}`,
-        resource: binary,
-      })
-    }
-
-
-   const  filteredEntry = entry.filter(el =>el.resource.resourceType !== "DocumentReference")
-
- 
-
+    await this.getAllSectionAndAllEntries(0, sectionEntries);
     const body = {
-      "resourceType": "Bundle",
-      "id": options.id,
-      "meta": {
-        "lastUpdated": new Date().toISOString()
+      resourceType: "Bundle",
+      id: options.id,
+      meta: {
+        lastUpdated: new Date().toISOString(),
       },
-      "identifier": {
-        "system": "https://www.nicehms.com/bundle",
-        "value": options.id
+      identifier: {
+        system: "https://www.nicehms.com/bundle",
+        value: options.id,
       },
-      "type": "document",
-      "timestamp": options.composition.date,
-      "entry": filteredEntry
-    }
+      type: "document",
+      timestamp: options.composition.date,
+      entry: this.entry,
+    };
+
+    const medicationRef = body.entry
+      .filter((el) => el.resource.resourceType == "MedicationRequest")
+      .map((el) => {
+        return {
+          reference: `MedicationRequest/${el.resource.id}`,
+        };
+      });
+   
+      let  sectionZeroNoMedicationRequest:{reference : string, type:string}[]=[]
+      sectionZeroNoMedicationRequest = body.entry[0].resource.section[0].entry.filter((el:any)=>el.type !="MedicationRequest" )
+
+      medicationRef.forEach(el=>{
+        sectionZeroNoMedicationRequest.push({
+          "reference" : el.reference,
+          "type" : "MedicationRequest"
+        })
+      })
+
+      body.entry[0].resource.section[0].entry=sectionZeroNoMedicationRequest
+
+    // const  filteredEntry =body.entry.filter(el =>el.resource.resourceType !== "DocumentReference")
+    // body.entry=filteredEntry;
     return body;
   }
 
