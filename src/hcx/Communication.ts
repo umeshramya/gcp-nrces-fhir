77fd68fddc1f224bcffa598fdb47017ee160978d
umeshramya <umeshbilagi@gmail.com>
2024-03-23 11:48:21 +0530

    communication

---
+++ src/hcx/Communication.ts
@@ -0,0 +1,82 @@
+import { CODEABLE_CONCEPT, IDENTTIFIER } from "../config";
+import { ResourceMaster } from "../Interfaces";
+import ResourceMain from "../resources/ResourceMai";
+
+export interface COMMUNICATION {
+  id?: string;
+  hcx: "nhcx" | "swasth";
+  text: string;
+  identifier: IDENTTIFIER[];
+  basedOnCommunicationIds?: string[];
+  status:
+    | "preparation"
+    | "in-progress"
+    | "not-done"
+    | "on-hold"
+    | "stopped"
+    | "completed"
+    | "entered-in-error"
+    | "unknown";
+  category: CODEABLE_CONCEPT[];
+  priority: "routine" | "urgent" | "asap" | "stat";
+  recipientOrganizationIds: string[];
+  senderOrganizationId: string;
+  contentBase64PDFstrings : {pdf :string, createdDate:string, title:string}[]
+}
+
+export default class Communication
+  extends ResourceMain
+  implements ResourceMaster
+{
+  getFHIR(options: COMMUNICATION) {
+    const body = {
+      resourceType: "Communication",
+      id: options.id ? options.id : undefined,
+      meta: {
+        versionId: "1",
+        lastUpdated: "2023-09-07T14:58:58.181+05:30",
+        profile:
+          options.hcx == "nhcx"
+            ? [
+                "https://nrces.in/ndhm/fhir/r4/StructureDefinition/Communication",
+              ]
+            : [
+                "	https://ig.hcxprotocol.io/v0.8/StructureDefinition-Communication.html",
+              ],
+      },
+      text: {
+        status: "generated",
+        div: options.text,
+      },
+      identifier: options.identifier,
+      basedOn: options.basedOnCommunicationIds?.map((el) => ({
+        reference: `CommunicationRequest/${el}`,
+      })),
+      status: options.status,
+      category: options.category,
+      priority: options.priority,
+      recipient: options.recipientOrganizationIds.map((el) => ({
+        reference: `Organization/${el}`,
+      })),
+      sender: {
+        reference: `Organization/${options.senderOrganizationId}`,
+      },
+      payload: {
+        contentAttachment: options.contentBase64PDFstrings.map(el=>(
+          {
+           contentType: "application/pdf",
+           language: "en-IN",
+           data: el.pdf,
+           title: el.title,
+           creation: el.createdDate
+         }
+       ))
+      }
+    };
+    return body;
+  }
+  convertFhirToObject(options: any) {
+    throw new Error("Method not implemented.");
+  }
+  statusArray?: Function | undefined;
+}
